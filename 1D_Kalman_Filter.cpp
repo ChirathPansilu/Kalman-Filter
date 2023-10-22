0a7c34b87ae27326195c80a0612eb49f509d87c2
Chirath Pansilu <chirathpansilu@gmail.com>
2023-10-22 21:44:31 +0530

    State prediction step added

--- 1D_Kalman_Filter.cpp
+++ 1D_Kalman_Filter.cpp
@@ -18,6 +18,13 @@ tuple<double, double> measurement_update(double mean1, double var1, double mean2
     return make_tuple(new_mean, new_var);
 }
 
+tuple<double, double> state_prediction(double mean1, double var1, double mean2, double var2)
+{
+    double new_mean = mean1 + mean2;
+    double new_var = var1 + var2;
+    return make_tuple(new_mean, new_var);
+}
+
 int main()
 {
     cout << "PDF value: " << f(10.0, 4.0, 8.0) << endl;
@@ -25,6 +32,9 @@ int main()
     double new_mean{}, new_var{};
 
     tie(new_mean, new_var) = measurement_update(10, 8, 13, 2);
+    printf("[%f, %f]\n", new_mean, new_var);
+
+    tie(new_mean, new_var) = state_prediction(10, 4, 12, 4);
     printf("[%f, %f]\n", new_mean, new_var);
     
     return 0;
