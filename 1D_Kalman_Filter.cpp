b39c0ed928353e1ce2be47b7d5b22b30e2babf10
Chirath Pansilu <chirathpansilu@gmail.com>
2023-10-22 21:42:46 +0530

    Measurement update step added

--- 1D_Kalman_Filter.cpp
+++ 1D_Kalman_Filter.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <tuple>
 #include <math.h>
-#include <corecrt_math_defines.h>
+// #include <corecrt_math_defines.h>
 
 using namespace std;
 
@@ -10,8 +11,21 @@ double f(double mu, double sigma2, double x)
     return prob;
 }
 
+tuple<double, double> measurement_update(double mean1, double var1, double mean2, double var2) {
+    double new_mean = (mean1 * var2 + mean2 * var1) / (var1 + var2);
+    double new_var = 1 / (1 / var1 + 1 / var2);
+
+    return make_tuple(new_mean, new_var);
+}
+
 int main()
 {
     cout << "PDF value: " << f(10.0, 4.0, 8.0) << endl;
+
+    double new_mean{}, new_var{};
+
+    tie(new_mean, new_var) = measurement_update(10, 8, 13, 2);
+    printf("[%f, %f]\n", new_mean, new_var);
+    
     return 0;
 }
